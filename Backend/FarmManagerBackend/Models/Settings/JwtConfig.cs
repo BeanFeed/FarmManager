e27df533d8b0bf62c6bf5e89dd54bc69b546ff15
BeanFeed <two.austin.two@gmail.com>
2024-07-04 14:20:44 -0400

    Change secure to boolean

--- Backend/FarmManagerBackend/Models/Settings/JwtConfig.cs
+++ Backend/FarmManagerBackend/Models/Settings/JwtConfig.cs
@@ -6,6 +6,6 @@ public class JwtConfig
     public string RKey { get; set; } = null!;
     public string Issuer { get; set; } = null!;
     public string Audience { get; set; } = null!;
-    public string Secure { get; set; } = null!;
+    public bool Secure { get; set; }
     public string SSM { get; set; } = null!;
 }
