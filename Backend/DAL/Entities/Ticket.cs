d341283ea966367be6f3755bc4691c9ec0be8002
BeanFeed <two.austin.two@gmail.com>
2024-07-04 22:12:00 -0400

    Make certain fields nullable

--- Backend/DAL/Entities/Ticket.cs
+++ Backend/DAL/Entities/Ticket.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DAL.Entities;
@@ -6,11 +7,15 @@ public class Ticket
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    [Required]
     public User OpenedBy { get; set; } = null!;
-    public User Technician { get; set; } = null!;
+    public User? Technician { get; set; } = null!;
+    [Required]
     public DateTime DateOpened { get; set; }
-    public DateTime DateClosed { get; set; }
+    public DateTime? DateClosed { get; set; }
+    [Required]
     public string Issue { get; set; } = null!;
-    public string Repair { get; set; } = null!;
+    public string? Repair { get; set; } = null!;
+    [Required]
     public Printer Printer { get; set; } = null!;
 }
