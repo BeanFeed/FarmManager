426496c899d120352fbcaaa76a02ba470fd22737
BeanFeed <two.austin.two@gmail.com>
2024-07-07 17:14:07 -0400

    Use lowercase urls

--- Backend/FarmManagerBackend/Program.cs
+++ Backend/FarmManagerBackend/Program.cs
@@ -46,6 +46,9 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
