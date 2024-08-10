da709f33381811d662528cdcc10646bbdb297bee
BeanFeed <two.austin.two@gmail.com>
2024-08-09 21:01:34 -0400

    Add loading icon.

--- Frontend/src/views/Printer/PrinterModifyMenu.vue
+++ Frontend/src/views/Printer/PrinterModifyMenu.vue
@@ -17,7 +17,11 @@ let locationName = ref("");
 let purchaseDate = ref("");
 
 let locationOptions = ref([])
+let printerBrands = ref([]);
+let modelOptions = ref([]);
 
+let loading = ref(false);
+let delloading = ref(false);
 
 onMounted(() => {
   let locationReq =  axios.get(backendUrl + "/api/ticket/getlocations", {withCredentials: true}).then(response => {
@@ -34,7 +38,41 @@ onMounted(() => {
       router.push('/login');
     }
   });
-})
+  let printerBrandReq = axios.get(backendUrl + "/api/printer/getprintertypevariants", {withCredentials: true}).then(response => {
+    printerBrands.value = response.data;
+  }).catch(error => {
+    toast(error.response.data.length < 30 ? error.response.data : error.body, {
+      "type": "error",
+      "closeOnClick": true,
+      "autoClose": 2000,
+      "pauseOnFocusLoss": false,
+      "transition": "bounce"
+    });
+    if (error.response.data === "User not logged in" || error.response.data === "Session Invalid") {
+      router.push('/login');
+    }
+  });
+});
+
+watch(brand, () => {
+  if (brand.value === "" || brand.value === "custom") return;
+
+  let printerReq = axios.get(backendUrl + "/api/printer/getprintertypevariants?modelByBrand=" + brand.value, {withCredentials: true}).then(response => {
+    modelOptions.value = response.data;
+  }).catch(error => {
+    toast(error.response.data.length < 30 ? error.response.data : error.body, {
+      "type": "error",
+      "closeOnClick": true,
+      "autoClose": 2000,
+      "pauseOnFocusLoss": false,
+      "transition": "bounce"
+    });
+    if (error.response.data === "User not logged in" || error.response.data === "Session Invalid") {
+      router.push('/login');
+    }
+  });
+});
+
 
 function submit() {
   let data = {
@@ -42,8 +80,10 @@ function submit() {
   }
   
   if (serialNumber.value !== "") data.serialNumber = serialNumber.value;
-  if (brand.value !== "") data.brand = brand.value;
-  if (model.value !== "") data.model = model.value;
+  if (brand.value !== "" && model.value !== "") {
+    data.brand = brand.value;
+    data.model = model.value;
+  }else if (brand.value !== "" && model.value === "") return;
   if (locationName.value !== "") data.locationName = locationName.value;
   if (purchaseDate.value !== "") data.purchaseDate = purchaseDate.value;
   
@@ -52,9 +92,12 @@ function submit() {
     return;
   }
   
+  loading.value = true;
+  
   let req = axios.post(backendUrl + "/api/printer/modifyprinter", data, {withCredentials: true}).then(response => {
     close();
   }).catch(error => {
+    loading.value = false;
     toast(error.response.data.length < 30 ? error.response.data : error.body, {
       "type": "error",
       "closeOnClick": true,
@@ -74,7 +117,8 @@ function close() {
   model = ref("");
   locationName = ref("");
   purchaseDate = ref("");
-  
+  loading.value = false;
+  delloading.value = false;
   emits('close');
 }
 
@@ -83,10 +127,11 @@ function deletePrinter() {
     close();
     return;
   }
-  
+  delloading.value = true;
   let req = axios.delete(backendUrl + "/api/printer/removeprinter?name=" + props.printer.name, {withCredentials: true}).then(response => {
     close();
   }).catch(error => {
+    delloading.value = false;
     toast(error.response.data.length < 30 ? error.response.data : error.message, {
       "type": "error",
       "closeOnClick": true,
@@ -119,22 +164,14 @@ function deletePrinter() {
       </div>
       <hr class="my-2">
       <p class="text-left">Brand</p>
-      <div class="max-w-4xl w-full rounded-lg bg-gray-200 mt-1">
-        <ul class="flex items-center">
-          <li class="mx-1 w-full">
-            <input type="text" v-model="brand" class="w-full text-green-500 focus:outline-none bg-gray-200">
-          </li>
-        </ul>
-      </div>
+      <select v-model="brand" class="text-green-500 p-1 bg-gray-200 rounded-lg w-full">
+        <option v-for="(brand, index) in printerBrands" :value="brand">{{brand}}</option>
+      </select>
       <hr class="my-2">
-      <p class="text-left">Model</p>
-      <div class="max-w-4xl w-full rounded-lg bg-gray-200 mt-1">
-        <ul class="flex items-center">
-          <li class="mx-1 w-full">
-            <input type="text" v-model="model" class="w-full text-green-500 focus:outline-none bg-gray-200">
-          </li>
-        </ul>
-      </div>
+      <p class="text-left">Model <span v-if="brand !==''" class="text-red-500">Required</span></p>
+      <select v-model="model" class="text-green-500 p-1 bg-gray-200 rounded-lg w-full">
+        <option v-for="(model, index) in modelOptions" :value="model">{{model}}</option>
+      </select>
       <hr class="my-2">
       <p class="text-left">Location</p>
       <select v-model="locationName" class="text-green-500 p-1 bg-gray-200 rounded-lg w-full">
@@ -152,9 +189,39 @@ function deletePrinter() {
       </div>
       <hr class="my-2">
       <div class="flex justify-center">
-        <input class="w-full bg-green-500 hover:bg-green-600 cursor-pointer p-2 rounded-lg mr-1 text-white" type="submit" value="Submit">
+        <button class="w-full bg-green-500 text-white p-2 rounded-lg enabled:cursor-pointer hover:bg-green-600 disabled:bg-green-700" type="submit" :disabled="loading || delloading">
+          <template v-if="!loading">
+            Submit
+          </template>
+          <template v-else>
+            <div class="text-center">
+              <div role="status">
+                <svg aria-hidden="true" class="inline w-5 h-5 text-green-500 animate-spin fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                </svg>
+                <span class="sr-only">Loading...</span>
+              </div>
+            </div>
+          </template>
+        </button>
         <input @click="close" class="w-full bg-gray-200 hover:bg-gray-300 cursor-pointer p-2 rounded-lg mx-1" type="button" value="Cancel">
-        <input @click="deletePrinter" class="w-full bg-red-500 hover:bg-red-600 cursor-pointer p-2 rounded-lg ml-1 text-white" type="button" value="Delete">
+        <button class="w-full bg-red-500 hover:bg-red-600 disabled:bg-red-700 enabled:cursor-pointer p-2 rounded-lg ml-1 text-white" @click="deletePrinter" :disabled="loading || delloading">
+          <template v-if="!delloading">
+            Delete
+          </template>
+          <template v-else>
+            <div class="text-center">
+              <div role="status">
+                <svg aria-hidden="true" class="inline w-5 h-5 text-red-500 animate-spin fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                </svg>
+                <span class="sr-only">Loading...</span>
+              </div>
+            </div>
+          </template>
+        </button>
       </div>
     </form>
   </div>
--- Frontend/src/views/Settings/UserModifyMenu.vue
+++ Frontend/src/views/Settings/UserModifyMenu.vue
@@ -15,6 +15,9 @@ let newName = ref("");
 let newRole = ref("");
 let newPassword = ref("");
 
+let loading = ref(false);
+let delloading = ref(false);
+
 function submit() {
   let data = {
     id: props.user.id
@@ -23,10 +26,11 @@ function submit() {
   if (newName.value !== "") data.name = newName.value;
   if (newRole.value !== "") data.role = newRole.value;
   if (newPassword.value !== "") data.password = newPassword.value;
-  
+  loading.value = true;
   let req = axios.post(backendUrl + "/api/user/modifyuser", data, {withCredentials: true}).then(response => {
     close();
   }).catch(error => {
+    loading.value = false;
     toast(error.response.data.length < 30 ? error.response.data : error.message, {
       "type": "error",
       "closeOnClick": true,
@@ -46,10 +50,11 @@ function deleteUser() {
     close();
     return;
   }
-  
+  delloading.value = true;
   let req = axios.delete(backendUrl + "/api/user/deleteuser?userId=" + props.user.id,  {withCredentials: true}).then(response => {
     close();
   }).catch(error => {
+    delloading.value = false;
     toast(error.response.data.length < 30 ? error.response.data : error.message, {
       "type": "error",
       "closeOnClick": true,
@@ -64,6 +69,8 @@ function close() {
   let newName = ref("");
   let newRole = ref("");
   let newPassword = ref("");
+  loading.value = false;
+  delloading.value = false;
   
   emits('close');
 }
@@ -107,9 +114,39 @@ function close() {
       </template>
       
       <div class="flex justify-center">
-        <input ref="submitButton" class="w-full bg-green-500 hover:bg-green-600 cursor-pointer p-2 rounded-lg mr-1 text-white" type="submit" value="Submit">
+        <button type="submit" class="w-full bg-green-500 hover:bg-green-600 disabled:bg-green-700 enabled:cursor-pointer p-2 rounded-lg mr-1 text-white" :disabled="loading || delloading">
+          <template v-if="!loading">
+            Submit
+          </template>
+          <template v-else>
+            <div class="text-center">
+              <div role="status">
+                <svg aria-hidden="true" class="inline w-5 h-5 text-green-500 animate-spin fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                </svg>
+                <span class="sr-only">Loading...</span>
+              </div>
+            </div>
+          </template>
+        </button>
         <input @click="close" class="w-full bg-gray-200 hover:bg-gray-300 cursor-pointer p-2 rounded-lg mx-1" type="button" value="Cancel">
-        <input @click="deleteUser" class="w-full bg-red-500 hover:bg-red-600 cursor-pointer p-2 rounded-lg ml-1 text-white" type="button" value="Delete" v-if="user.role !== 'Owner'">
+        <button @click="deleteUser" @submit.prevent="" type="button" class="w-full bg-red-500 hover:bg-red-600 disabled:bg-red-700 enabled:cursor-pointer p-2 rounded-lg ml-1 text-white" v-if="user.role !== 'Owner'" :disabled="loading || delloading">
+          <template v-if="!delloading">
+            Delete
+          </template>
+          <template v-else>
+            <div class="text-center">
+              <div role="status">
+                <svg aria-hidden="true" class="inline w-5 h-5 text-red-500 animate-spin fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                </svg>
+                <span class="sr-only">Loading...</span>
+              </div>
+            </div>
+          </template>
+        </button>
       </div>
     </form>
   </div>
